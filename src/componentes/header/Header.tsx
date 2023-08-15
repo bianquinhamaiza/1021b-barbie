aeace1420c00e970e4255b638bf8ca834a541ab4
MAIZA BIANCA DIAS GUILHERME <06981474131@ifms.edu.br>
2023-08-15 07:37:12 -0400

    first commit

---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,19 @@
+import './Header.css'
+function Header(){
+    return(
+        <header>
+            <div>
+                Tere Careca
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Ajude</a></li>
+                    <li><a href="#">Esse</a></li>
+                    <li><a href="#">Pobre</a></li>
+                    <li><a href="#">Coitado</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header
